5fb449a69b7b94cf6a5718a2d52fa468f9f5a79c
乔宏宇 <2448534412@qq.com>
2024-11-06 15:23:44 +0800

    1

--- src/pages/dynamic/dynamic.vue
+++ src/pages/dynamic/dynamic.vue
@@ -1,5 +1,16 @@
 <template>
-    <view>动态</view>
+    <view class="item">
+        <view class="title">
+            <image src=""></image>
+            <text>用户id</text>
+        </view>
+        <view class="content">
+            <text>雨后天晴</text>
+        </view>
+        <view class="bar">
+            
+        </view>
+    </view>
 </template>
 
 <script lang="ts" setup>
@@ -8,5 +19,24 @@ import { ref } from "vue"
 </script>
 
 <style lang="scss" scoped>
-
+    .item{
+        min-height:80px;
+        border-bottom:1px solid #eee;
+        padding:10px;
+        .title{
+            display: flex;
+            align-items: center;
+            height:50px;
+            image{
+                width:50px;
+                height:50px;
+                border:1px solid #eee;
+                border-radius:50%;
+                margin-right:15px;
+            }
+        }
+        .content{
+            padding-left:65px;
+        }
+    }
 </style>
