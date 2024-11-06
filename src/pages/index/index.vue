a88e38d93013b4edc9dcfc6bf3be21e0640132ce
张振青 <2159682735@qq.com>
2024-11-06 16:57:45 +0800

    12

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,14 @@
 <template>
     <view>
-      <view class="iconfont icon-gengduo">123</view>
+      <view class="iconfont icon-gengduo"></view>
     </view>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue" 
+import {getBanner} from '../../servers/servers'
+
+getBanner().then(res => console.log(res.data))
 
 
 </script>
