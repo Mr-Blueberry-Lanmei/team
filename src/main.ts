18510ff8ffc61bee9a8e5e103a3e74ce50cd6a7c
张振青 <2159682735@qq.com>
2024-11-07 17:04:17 +0800

    zz

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createSSRApp } from "vue";
 import App from "./App.vue";
 import * as Pinia from 'pinia'
 import './download (2)/font_310irbem5bp/iconfont.css'
+import './download (2) (2)/font_nnk1caaundf/iconfont.css'
 
 export function createApp() {
   const app = createSSRApp(App);
