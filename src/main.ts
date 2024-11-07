83345187a28e53817ea0a907feadaffd4d491611
曹珂萌 <2383384550@qq.com>
2024-11-07 17:08:31 +0800

    主暂存

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,12 @@ import { createSSRApp } from "vue";
 import App from "./App.vue";
 import * as Pinia from 'pinia'
 import './download (2)/font_310irbem5bp/iconfont.css'
-<<<<<<< HEAD
 import './download (2) (2)/font_nnk1caaundf/iconfont.css'
 
 
 export function createApp() {
   const app = createSSRApp(App)
   app.use(Pinia.createPinia())
-=======
-import * as Pinia from 'pinia';
-
-export function createApp() {
-  const app = createSSRApp(App);
-  app.use(Pinia.createPinia());
->>>>>>> ckm
   return {
     app,
     Pinia
