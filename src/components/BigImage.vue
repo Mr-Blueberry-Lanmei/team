c7d1fc9660951f1f74372e4dde30b88ff40abba8
曹珂萌 <2383384550@qq.com>
2024-11-08 23:45:08 +0800

    更新

---
+++ src/components/BigImage.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {defineProps} from "vue"
+const props=defineProps(["url"]);
+function backPage(){
+    uni.navigateBack()
+}
+</script>
+
+<template>
+    <view class="imgBox">
+        <text @click="backPage">&times;</text>
+        <view class="imgPop">
+            <image src="https://th.bing.com/th/id/OIP.3r1vguZyWFUJ80A2Nf2k3AHaEK?rs=1&pid=ImgDetMain" mode="widthFix"></image>
+        </view>
+    </view>
+</template>
+
+<style scoped lang="scss">
+    .imgBox{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: black;
+        position: fixed;
+        top:0;
+        left:0;
+        z-index: 999;
+        >text{
+            position: absolute;
+            color: #fff;
+            width: 100rpx;
+            height: 100rpx;
+            font-size: 100rpx;
+            line-height: 100rpx;
+            text-align: center;
+            font-weight: 500;
+            top: 20rpx;
+            left: 20rpx;
+        }
+        >view{
+            >image{
+            flex: none;
+            width: 100vw !important;
+            }
+        }
+    }
+</style>
